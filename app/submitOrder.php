73b6ca97292258f84e03c3e97062f1c2b3b80254
corinnaerin <corinnaerin@localhost>
2013-04-22 15:33:34 -0400

    fix IE bootstrapping issue, fix some css issues with the horrid template code, fix empty body of e-mail

--- app/submitOrder.php
+++ app/submitOrder.php
@@ -5,11 +5,12 @@
  
 $data = file_get_contents("php://input");
  
-$objData = json_decode($data);
+//$objData = json_decode($data);
 
 $to = "orders@bedrocklearning.com";
+//$to = "corinnaerin@gmail.com";
 
-if (mail($to, "Purchase Order", "$objData->message", "Content-Type: text/html; charset=ISO-8859-1\r\n")) {
+if (mail($to, "Purchase Order", "$data", "Content-Type: text/html; charset=ISO-8859-1\r\n")) {
     echo "Order submitted successfully";
 } else {
     echo "An error occurred while submitting the order";
