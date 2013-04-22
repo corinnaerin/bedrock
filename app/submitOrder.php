4f1b9eed9eaa72bb1c2c85b7665e46ab42bf9aa7
Corinna Schmidt <corinnaerin@users.noreply.github.com>
2013-04-22 00:33:20 -0400

    add back-end

---
+++ app/submitOrder.php
@@ -0,0 +1,18 @@
+<?php
+// The request is a JSON request.
+// We must read the input.
+// $_POST or $_GET will not work!
+ 
+$data = file_get_contents("php://input");
+ 
+$objData = json_decode($data);
+
+$to = "orders@bedrocklearning.com";
+
+if (mail($to, "Purchase Order", "$objData->message", "Content-Type: text/html; charset=ISO-8859-1\r\n")) {
+    echo "Order submitted successfully";
+} else {
+    echo "An error occurred while submitting the order";
+}
+
+?>
